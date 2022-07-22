a6cf212dae6719f618184d1ab1db7b6adbdd4b37
Gabriel Chagas <gabrielserq@outlook.com>
2022-07-21 22:50:20 -0300

    chore: add index.js modules 2 class 10.

--- 02_fundamentos/10_abstracao_input/index.js
+++ 02_fundamentos/10_abstracao_input/index.js
@@ -1,1 +1,14 @@
 const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+inquirer.prompt([{
+    name: 'p1',
+    message: 'Qual a primeira nota: '
+}, {
+    name: 'p2',
+    message: 'Qual a primeira nota: '
+}]).then((answers) => {
+    let avarage = ((parseInt(answers.p1) + parseInt(answers.p2)) / 2);
+    avarage >= 7 ? console.log(chalk.green('Parabéns, você foi aprovado!')) : console.log(chalk.red('Você foi reprovado!'));
+
+}).catch(err => console.log(err));
