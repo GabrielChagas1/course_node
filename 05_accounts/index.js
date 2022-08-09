b514d5ca602e480fba7946c48ae27f75b389d2c8
Gabriel Chagas <gabrielserq@outlook.com>
2022-08-09 19:46:19 -0300

    chore: creating function to create account.

--- 05_accounts/index.js
+++ 05_accounts/index.js
@@ -6,3 +6,10 @@ const fs = require('fs');
 
 operation();
 
+// create an account 
+function createAccount(){
+    console.log(chalk.bgGreen.black(`Parabéns por escolher nosso banco - GM Bank`));
+    console.log(chalk.green(`Defina as opções da sua conta a seguir`));
+    buildAccount();
+}
+
