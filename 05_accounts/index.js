ac7aee12c3b4e085f5ae36097cf675397fa296ab
Gabriel Chagas <gabrielserq@outlook.com>
2022-08-09 19:46:53 -0300

    chore: creating function to build the account.

--- 05_accounts/index.js
+++ 05_accounts/index.js
@@ -13,3 +13,30 @@ function createAccount(){
     buildAccount();
 }
 
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta: '
+        }
+    ]).then((answer) => {
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) fs.mkdirSync('accounts');
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black(`Está conta já existe, escolha outro nome!`))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, `{"balance": 0}`, (err) => {
+            console.log(err)
+        })
+
+        console.log(chalk.green(`Parabéns, a sua conta foi criado com sucesso`))
+        operation()
+
+    }).catch((err) => console.log(err))
+}
