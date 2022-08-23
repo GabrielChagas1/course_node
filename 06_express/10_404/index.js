9641862885f816e4a16f6690e97e0b307e2eaadf
Gabriel Chagas <gabrielserq@outlook.com>
2022-08-23 12:25:16 -0300

    modules 06 class 10 index.js

---
+++ 06_express/10_404/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3333
+
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+const users = require('./users')
+
+app.use('/users', users)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.use(function(req, res, next){
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
