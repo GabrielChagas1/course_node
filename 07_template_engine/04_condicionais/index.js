c14cc29459abf09e285918f499b04551430cb367
Gabriel Chagas <gabrielserq@outlook.com>
2022-08-31 16:52:51 -0300

    chore: modules 07 class 04 index.js

---
+++ 07_template_engine/04_condicionais/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+
+    const user = {
+        name: "Gabriel",
+        surname: "Chagas",
+        age: '30'
+    }
+
+    const auth = false
+
+    res.render('home', {user, auth})
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+app.listen(3000, () => console.log(`Projeto rodando na porta 3000`))
