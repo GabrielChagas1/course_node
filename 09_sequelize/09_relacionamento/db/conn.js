dd21f3fc8551ceb8ab91e345945a904ae40bc6d7
Gabriel Chagas <gabrielserq@outlook.com>
2023-01-30 20:44:14 -0300

    chore: modules 09 class 09 conn.js

--- 09_sequelize/09_relacionamento/db/conn.js
+++ 09_sequelize/09_relacionamento/db/conn.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('nodesequelize2', 'root', '', {
+const sequelize = new Sequelize('nodesequelize2', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql'
 })
