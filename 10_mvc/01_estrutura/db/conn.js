1d47466ec7ce44343e335705affbb7e840b18088
Gabriel Chagas <gabrielserq@outlook.com>
2023-02-03 21:03:11 -0300

    chore: modules 10 class 01 conn.js

---
+++ 10_mvc/01_estrutura/db/conn.js
@@ -0,0 +1,17 @@
+const {Sequelize} = require('sequelize')
+
+require('dotenv').config()
+
+const sequelize = new Sequelize('nodemvc2', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+})
+
+try {
+    sequelize.authenticate();
+    console.log('Conectamos ao MySQL!')
+} catch (error) {
+    console.log(`Não foi possível conectar: ${error}`)
+}
+
+exports.default = sequelize
