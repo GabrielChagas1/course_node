77c1f3ca6b2a4a11260408704b505040d001f6ba
Gabriel Chagas <gabrielserq@outlook.com>
2023-02-23 16:28:17 -0300

    feat: adding session-file-store

--- 11_toughts/index.js
+++ 11_toughts/index.js
@@ -1,3 +1,4 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const FileStore = require('session-file-store')(session)
