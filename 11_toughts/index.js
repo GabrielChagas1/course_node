c52434b9596996ac513980ce96a2f3110b01db08
Gabriel Chagas <gabrielserq@outlook.com>
2023-02-23 16:29:35 -0300

    feat: initializing the project

--- 11_toughts/index.js
+++ 11_toughts/index.js
@@ -4,3 +4,5 @@ const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 require('dotenv').config()
+
+const app = express()
