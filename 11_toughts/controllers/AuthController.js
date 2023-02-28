4c993fccf95a8a439bd8d60ca1ca940c0f3e0436
Gabriel Chagas <gabrielserq@outlook.com>
2023-02-28 16:37:35 -0300

    feat: create module registerPost

--- 11_toughts/controllers/AuthController.js
+++ 11_toughts/controllers/AuthController.js
@@ -36,3 +36,50 @@ const bcrypt = require('bcryptjs')
     static register(req, res){
         res.render('auth/register')
     }
+
+    static async registerPost(req, res){
+        const {name, email, password, confirmpassword} = req.body
+
+        // password match validation
+        if(password != confirmpassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+            return
+        }
+
+        // check if user exists
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+        if(checkIfUserExists){
+            req.flash('message', 'O e-mail já está em uso!')
+            res.render('auth/register')
+            return
+        }
+
+        // create a password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name, 
+            email,
+            password: hashedPassword
+        }
+
+        try {
+            const createUser = await User.create(user)
+
+            // Initialize session
+            req.session.userid = createUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso!')
+
+            req.session.save(() => {
+                //res.render('toughts/home')
+                res.redirect('/')
+            })
+
+        } catch (error) {
+            console.log(`Erro na hora de cadastrar o usuário ${error}`)
+        }
+
+    }
