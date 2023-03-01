39964ca7e81e2b1c3964744ebd5fc1ebb4364f68
Gabriel Chagas <gabrielserq@outlook.com>
2023-03-01 16:41:09 -0300

    feat: creating function showToughts

--- 11_toughts/controllers/ToughtController.js
+++ 11_toughts/controllers/ToughtController.js
@@ -5,3 +5,28 @@ const {Op} = require('sequelize')
 
 module.exports = class ToughtController {
     static async showToughts(req, res){
+
+        let search = ''
+
+        if(req.query.search) search = req.query.search
+
+        let order = 'DESC'
+
+        req.query.order === 'old' ? order = 'ASC' : order = 'DESC'
+
+        const toughtsData = await Tought.findAll({
+            include: User, 
+            where: {
+                title:{[Op.like]: `%${search}%`}
+            },
+            order: [['createdAt', order]]
+        })
+
+        const toughts = toughtsData.map((result) => result.get({plain: true}))
+
+        let toughtsQty = toughts.length
+
+        if(toughtsQty === 0) toughtsQty = false
+
+        res.render('toughts/home', {toughts, search, toughtsQty})
+    }
