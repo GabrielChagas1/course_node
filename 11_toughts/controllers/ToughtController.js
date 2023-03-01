d83e42e0900d394730014baf2b7a46f818cc353f
Gabriel Chagas <gabrielserq@outlook.com>
2023-03-01 16:40:18 -0300

    chore: creating class ToughtController.

--- 11_toughts/controllers/ToughtController.js
+++ 11_toughts/controllers/ToughtController.js
@@ -2,3 +2,6 @@ const Tought = require('../models/Tought')
 const User = require('../models/User')
 
 const {Op} = require('sequelize')
+
+module.exports = class ToughtController {
+    static async showToughts(req, res){
