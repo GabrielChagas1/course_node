483140dd989a9ff58e8ccae9dfee9af719fc2d96
Gabriel Chagas <gabrielserq@outlook.com>
2023-03-04 16:44:49 -0300

    feat: creat const for datas connection bd.

--- 11_toughts/db/conn.js
+++ 11_toughts/db/conn.js
@@ -1,3 +1,8 @@
 const {Sequelize} = require('sequelize')
 
 require('dotenv').config()
+
+const sequelize = new Sequelize('toughts', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+})
