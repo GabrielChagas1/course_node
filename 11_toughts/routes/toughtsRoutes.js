9fa38289d0b8b02bac90d6308fa3790d490c7b64
Gabriel Chagas <gabrielserq@outlook.com>
2023-03-06 16:48:11 -0300

    feat: recovering checkAuth to the helper

--- 11_toughts/routes/toughtsRoutes.js
+++ 11_toughts/routes/toughtsRoutes.js
@@ -1,3 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const ToughtController = require('../controllers/ToughtController')
+
+// helper
+const checkAuth = require('../helpers/auth').checkAuth
