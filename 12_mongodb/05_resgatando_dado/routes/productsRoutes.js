416dfd0f5f3ffaea3a80b26cbd97d076270280f5
Gabriel Chagas <gabrielserq@outlook.com>
2023-03-20 20:23:02 -0300

    chore: class 12 module 05 productsRoutes.js

---
+++ 12_mongodb/05_resgatando_dado/routes/productsRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const ProductControlller = require('../controllers/ProductController')
+
+router.get('/', ProductControlller.showProducts)
+router.get('/create', ProductControlller.createProduct)
+router.post('/create', ProductControlller.createProductPost)
+router.get('/:id', ProductControlller.getProduct)
+router.post('/remove/:id', ProductControlller.removeProduct)
+
+
+
+module.exports = router
