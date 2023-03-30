473e54ccfd06e75e5e07c5353209e3ff34dac3bb
Gabriel Chagas <gabrielserq@outlook.com>
2023-03-30 15:45:43 -0300

    chore: create index.js 11 thoughts

--- 11_toughts/index.js
+++ 11_toughts/index.js
@@ -3,7 +3,6 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
-require('dotenv').config()
 
 const app = express()
 
