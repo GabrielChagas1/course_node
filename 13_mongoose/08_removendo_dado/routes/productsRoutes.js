4c791fba4b6738e4b3d16d265ab91c6a442ffe14
Gabriel Chagas <gabrielserq@outlook.com>
2023-04-13 16:33:41 -0300

    chore: module 13 class 08 productsRoutes.js

---
+++ 13_mongoose/08_removendo_dado/routes/productsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const ProductControlller = require('../controllers/ProductController')
+
+router.get('/', ProductControlller.showProducts)
+router.get('/create', ProductControlller.createProduct)
+router.post('/create', ProductControlller.createProductPost)
+router.get('/:id', ProductControlller.getProduct)
+router.post('/remove/:id', ProductControlller.removeProduct)
+router.get('/edit/:id', ProductControlller.editProduct)
+router.post('/edit', ProductControlller.editProductPost)
+
+
+
+module.exports = router
