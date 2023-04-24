ac5fcec6d2f32d8c1fa57b12b14fa1097c20c2a1
Gabriel Chagas <gabrielserq@outlook.com>
2023-04-24 16:58:13 -0300

    chore: config solve cors.

--- 15_get_a_pet/backend/index.js
+++ 15_get_a_pet/backend/index.js
@@ -6,3 +6,7 @@ const app = express()
 // config json response
 app.use(express.json())
 
+// solve cors
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+
+app.use(express.static('public'))
