2fd0cb1fefb949835e322fd435a45df90c27e7ba
Gabriel Chagas <gabrielserq@outlook.com>
2023-04-24 17:04:56 -0300

    chore: creating controller getAllUserPets

--- 15_get_a_pet/backend/controllers/PetController.js
+++ 15_get_a_pet/backend/controllers/PetController.js
@@ -83,3 +83,17 @@ const ObjectId = require('mongoose').Types.ObjectId
             pets: pets
         })
     }
+
+    static async getAllUserPets(req, res) {
+        // get user from token
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({
+            'user._id': user._id
+        }).sort('-createdAt')
+
+        res.status(200).json({
+            pets
+        })
+    }
