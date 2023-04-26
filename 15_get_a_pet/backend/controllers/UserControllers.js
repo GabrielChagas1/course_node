b3b44dce8e4d2a4959883c6b9cb788be3c0d2219
Gabriel Chagas <gabrielserq@outlook.com>
2023-04-26 17:10:14 -0300

    chore: recovering the libraries UserController.js

--- 15_get_a_pet/backend/controllers/UserControllers.js
+++ 15_get_a_pet/backend/controllers/UserControllers.js
@@ -1,3 +1,7 @@
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 // helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
