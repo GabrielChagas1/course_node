3537f8b22421c870b774fadafc44d1fa566e2811
Gabriel Chagas <gabrielserq@outlook.com>
2023-04-28 17:16:59 -0300

    chore: create router getAll

--- 15_get_a_pet/backend/routes/PetRoutes.js
+++ 15_get_a_pet/backend/routes/PetRoutes.js
@@ -7,3 +7,4 @@ const verifyToken = require('../helpers/verify-token')
 const {imageUpload} = require('../helpers/image-upload')
 
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create)
+router.get('/', PetController.getAll)
