9cea521370ba1b598840e9f51fa49a7399b4a69a
Gabriel Chagas <gabrielserq@outlook.com>
2023-04-29 14:58:53 -0300

    chore: creating UserRoutes.js

--- 15_get_a_pet/backend/routes/UserRoutes.js
+++ 15_get_a_pet/backend/routes/UserRoutes.js
@@ -1,3 +1,7 @@
+const router = require('express').Router()
+
+const UserController = require('../controllers/UserControllers')
+
 // middleware
 const verifyToken = require('../helpers/verify-token')
 const {imageUpload} = require('../helpers/image-upload')
@@ -7,3 +11,5 @@ router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
 router.patch('/edit/:id', verifyToken, imageUpload.single('image'), UserController.editUser)
+
+module.exports = router
