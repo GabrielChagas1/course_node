60377adb9a1799d091779ae2df18f0e75f221a09
Gabriel Chagas <gabrielserq@outlook.com>
2023-05-04 14:54:39 -0300

    chore: creating fuction schedule PetDetails.js

--- 15_get_a_pet/frontend/src/components/pages/Pet/PetDetails.js
+++ 15_get_a_pet/frontend/src/components/pages/Pet/PetDetails.js
@@ -19,3 +19,24 @@ import useFlashMessage from '../../../hooks/useFlashMessage'
             setPet(response.data.pet)
         }, [id])
     })       
+    async function schedule() {
+        let msgType = 'success'
+    
+        const data = await api
+          .patch(`pets/schedule/${pet._id}`, {
+            headers: {
+              Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+          })
+          .then((response) => {
+            console.log(response.data)
+            return response.data
+          })
+          .catch((err) => {
+            console.log(err)
+            msgType = 'error'
+            return err.response.data
+          })
+    
+        setFlashMessage(data.message, msgType)
+    }
