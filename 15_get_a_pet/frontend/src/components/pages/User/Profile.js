dd466c758ed8cd84d535cd26da4ff6f7414ff053
Gabriel Chagas <gabrielserq@outlook.com>
2023-05-06 14:57:24 -0300

    chore: creating variables Profile

--- 15_get_a_pet/frontend/src/components/pages/User/Profile.js
+++ 15_get_a_pet/frontend/src/components/pages/User/Profile.js
@@ -6,3 +6,8 @@ import Input from '../../form/Input'
 import { useEffect, useState } from 'react'
 import useFlashMessage from '../../../hooks/useFlashMessage'
 import RoundedImage from '../../layout/RoundedImage'
+
+    const [user, setUser] = useState({})
+    const [preview, setPreview] = useState()
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
