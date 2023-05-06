b213a95081bda7b9e6e352b47f570b2e8786c2de
Gabriel Chagas <gabrielserq@outlook.com>
2023-05-06 14:57:02 -0300

    chore: import libraries Profile.js

---
+++ 15_get_a_pet/frontend/src/components/pages/User/Profile.js
@@ -0,0 +1,8 @@
+import api from '../../../utils/api'
+import formStyles from '../../form/Form.module.css'
+import styles from './Profile.module.css'
+
+import Input from '../../form/Input'
+import { useEffect, useState } from 'react'
+import useFlashMessage from '../../../hooks/useFlashMessage'
+import RoundedImage from '../../layout/RoundedImage'
