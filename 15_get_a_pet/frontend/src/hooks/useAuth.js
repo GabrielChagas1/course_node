3ef9937f2f3af7bfccffe303f931750b4f900ffc
Gabriel Chagas <gabrielserq@outlook.com>
2023-05-16 15:05:15 -0300

    chore: create variables useAuth.js

--- 15_get_a_pet/frontend/src/hooks/useAuth.js
+++ 15_get_a_pet/frontend/src/hooks/useAuth.js
@@ -4,3 +4,7 @@ import {useState,useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 import useFlashMessage from './useFlashMessage'
+
+    const {setFlashMessage} = useFlashMessage()
+    const [authenticated, setAuthenticated] = useState(false)
+    const navigate = useNavigate()
