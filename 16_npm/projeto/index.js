1091959d017e7dacf8cc372c610b5631a6bd8af9
Gabriel Chagas <gabrielserq@outlook.com>
2023-06-02 15:25:42 -0300

    chore: creating index.js

--- 16_npm/projeto/index.js
+++ 16_npm/projeto/index.js
@@ -1,1 +1,7 @@
 var request = require('request')
+
+request('http://www.google.com', (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    console.log('body:', body)
+})
